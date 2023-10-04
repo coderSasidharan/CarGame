466fc31cee228fc863c560e826d89cac480817c0
coderSasidharan <66076670+coderSasidharan@users.noreply.github.com>
2023-10-04 09:18:05 -0400

    Update Player.js

--- JS/Player.js
+++ JS/Player.js
@@ -1,5 +1,10 @@
 class Player{
     constructor(){
+        var options = {
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0
+        }
         this.name = null;
         this.index = 0;
         this.distance = 0;
