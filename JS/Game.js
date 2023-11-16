35498beacaa9f8b67cde8408feeae396a1c6556d
coderSasidharan <66076670+coderSasidharan@users.noreply.github.com>
2023-11-16 11:37:11 -0500

    Update Game.js

--- JS/Game.js
+++ JS/Game.js
@@ -43,7 +43,7 @@ class Game{
              background("green")
              image(track,0,-displayHeight*4,displayWidth,displayHeight*5)
              var index = 0; 
-             var x+200; 
+             var x = x+200; 
              var y;
              for(var plr in playerDetails){ 
                  index = index + 1 ; 
