24130956020f407b2438c1e171502b29fec673a0
coderSasidharan <66076670+coderSasidharan@users.noreply.github.com>
2023-11-27 11:11:47 -0500

    Update Game.js

--- JS/Game.js
+++ JS/Game.js
@@ -190,39 +190,39 @@ class Game{
               } 
 
 
-          if(player.index == 1 && car1.x<260){
+          if(player.index == 1 && car1.x<331){
               player.x +=13
               player.update();
           }
         console.log(car1.x);
-          if(player.index == 1 && car1.x>1280){
+          if(player.index == 1 && car1.x>1680){
               player.x -=13
               player.update();
           }
         
-        if(player.index == 2 && car2.x<260){
+        if(player.index == 2 && car2.x<331){
               player.x +=13
               player.update();
           }
-          if(player.index == 2 && car2.x>1280){
+          if(player.index == 2 && car2.x>1680){
               player.x -=13
               player.update();
           }
 
-        if(player.index == 3 && car3.x<260){
+        if(player.index == 3 && car3.x<331){
               player.x +=13
               player.update();
           }
-          if(player.index == 3 && car3.x>1280){
+          if(player.index == 3 && car3.x>1680){
               player.x -=13
               player.update();
           }
 
-        if(player.index == 4 && car4.x<260){
+        if(player.index == 4 && car4.x<331){
               player.x +=13
               player.update();
           }
-          if(player.index == 4 && car4.x>1280){
+          if(player.index == 4 && car4.x>1680){
               player.x -=13
               player.update();
           }
