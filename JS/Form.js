77bc1d1e806879d9568cb25d73a6b23ac54dfdd0
coderSasidharan <66076670+coderSasidharan@users.noreply.github.com>
2023-11-29 09:13:38 -0500

    Update Form.js

--- JS/Form.js
+++ JS/Form.js
@@ -30,10 +30,10 @@ class Form{
             player.update()
             player.updateCount(playerCount)
             
-            this.greeting.html("Details Uploaded "+player.name+ "Players: "+playerCount+"/4")
-            this.greeting.position(displayWidth/2-70,displayHeight/4)
-            this.rules.html("Use arrow Keys to move")
-            this.rules.position(displayWidth/2-70,displayHeight/4+50)
+            this.greeting.html("Details Uploaded "+player.name+ "   Players: "+playerCount+"/4")
+            this.greeting.position(displayWidth/2-100,displayHeight/4)
+            this.rules.html("Rules:\n Use arrow Keys to move\n Moving diagonally is faster ")
+            this.rules.position(displayWidth/2-100,displayHeight/4+50)
         })
         this.reset.mousePressed(()=>{
             game.updateGameState(0);
